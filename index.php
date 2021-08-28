83baa2739ddaa1d85bbcaf70b6e376335e195742
Mahmoud Kamal <61159931+Mahmoud-kamal12@users.noreply.github.com>
2021-08-28 03:10:25 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,27 @@
-<?php $api_url = 'https://www.themealdb.com/api/json/v1/1/categories.php'; $json_data = file_get_contents($api_url); $response_data = json_decode($json_data); $categories = $response_data->categories; $allMeals = []; foreach ($categories as $category) { $api_url = "https://www.themealdb.com/api/json/v1/1/filter.php?c=". $category->strCategory; $json_data = file_get_contents($api_url); $response_data = json_decode($json_data); $meals = $response_data->meals; foreach ($meals as $meal) { $meal->price = rand(100 , 600); array_push($allMeals , $meal); } } $allM = array_slice($allMeals, 2); print_r(json_encode($allM)); ?>
+<?php
+
+$api_url = 'https://www.themealdb.com/api/json/v1/1/categories.php';
+
+$json_data = file_get_contents($api_url);
+
+$response_data = json_decode($json_data);
+
+$categories = $response_data->categories;
+
+$allMeals = [];
+
+foreach ($categories as $category) {
+    $api_url = "https://www.themealdb.com/api/json/v1/1/filter.php?c=". $category->strCategory;
+    $json_data = file_get_contents($api_url);
+    $response_data = json_decode($json_data);
+    $meals = $response_data->meals;
+    foreach ($meals as $meal) {
+        $meal->price = rand(100 , 600);
+        array_push($allMeals , $meal);
+    }
+}
+$Ma = array_slice($allMeals , 0, 3);
+print_r($Ma);
+// print_r(json_encode($allMeals));
+
+?>
